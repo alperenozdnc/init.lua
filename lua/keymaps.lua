8a6083a7af7eea959324621cafe61e21e2e07087
alperenozdnc <alperenozdincc@gmail.com>
2025-04-15 20:09:29 +0300

    barebones: theme, some keymaps and a plugin manager

---
+++ lua/keymaps.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = " ";
+
+function keymap (mode, bind, action, opts)
+	vim.api.nvim_set_keymap(mode, bind, action, opts);
+end
+
+-- normal mode
+keymap("n", "<leader>w", ":w<CR>", { noremap = true }); -- writes changes
+keymap("n", "<leader>e", ":Ex<CR>", { noremap = true }); -- opens file explorer
+keymap("n", "<leader>q", ":q<CR>", { noremap = true }); -- opens file explorer
+
+-- config options
+keymap("n", "<leader>cl", ":so %<CR>", { noremap = true, silent = true }); -- loads config changes
+keymap("n", "<leader>ce", ":e /home/xeptao/.config/nvim<CR>", { noremap = true, silent = true }) -- opens config folder
+
+-- insert mode
+keymap("i", "jj", "<ESC>", { noremap = true, silent = true });
---
+++ lua/options.lua
@@ -0,0 +1,7 @@
+vim.opt.relativenumber = true;
+vim.opt.cursorline = true;
+vim.opt.tabstop = 4;
+vim.opt.shiftwidth = 4;
+vim.opt.expandtab = true;
+
+vim.cmd("colorscheme tokyonight");
