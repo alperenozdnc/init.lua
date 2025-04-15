c496f5f0db86013d85890b0ea9216a6f59b49e62
alperenozdnc <alperenozdincc@gmail.com>
2025-04-15 22:02:28 +0300

    some cool keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,6 +8,10 @@ end
 keymap("n", "<leader>w", ":w<CR>", { noremap = true }); -- writes changes
 keymap("n", "<leader>e", ":Ex<CR>", { noremap = true }); -- opens file explorer
 keymap("n", "<leader>q", ":q<CR>", { noremap = true }); -- opens file explorer
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]]) -- copy selection to keyboard 
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- config options
 keymap("n", "<leader>cl", ":so %<CR>", { noremap = true, silent = true }); -- loads config changes
@@ -15,3 +19,11 @@ keymap("n", "<leader>ce", ":e /home/xeptao/.config/nvim<CR>", { noremap = true, 
 
 -- insert mode
 keymap("i", "jj", "<ESC>", { noremap = true, silent = true });
+
+-- visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+keymap("v", "<Tab>", ">gv", { noremap = true, silent = true }) -- indent selection in
+keymap("v", "<S-Tab>", "<gv", { noremap = true, silent = true }) -- indent selection out
+
