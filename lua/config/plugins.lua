b867885d45a8911d3cb6baf51717554a5d08ca7e
alperenozdnc <alperenozdincc@gmail.com>
2025-05-18 18:11:51 +0300

    feat: include emmet support for HTML

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -15,34 +15,34 @@ local builtin = require('telescope.builtin');
 vim.keymap.set('n', '<leader>ff', builtin.find_files);
 vim.keymap.set('n', '<leader>fg', builtin.git_files);
 vim.keymap.set('n', '<leader>fs', function()
-    builtin.grep_string({ search = vim.fn.input("grep > ") });
+	builtin.grep_string({ search = vim.fn.input("grep > ") });
 end)
 
 require("telescope").setup {
-    extensions = {
-        ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-        }
-    }
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown {
+			}
+		}
+	}
 }
 
 require("telescope").load_extension("ui-select");
 
 -- treesitter
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "c", "lua", "python", "javascript", "typescript" },
-    sync_install = false,
-    auto_install = true,
+	ensure_installed = { "c", "lua", "python", "javascript", "typescript" },
+	sync_install = false,
+	auto_install = true,
 
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
 
-    indent = {
-        enable = true,
-    }
+	indent = {
+		enable = true,
+	}
 }
 
 -- undotree
@@ -55,7 +55,7 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 require("mason").setup();
 
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "clangd", "pyright", "eslint" }
+	ensure_installed = { "lua_ls", "clangd", "pyright", "eslint" }
 })
 
 local lspconfig = require("lspconfig");
@@ -64,65 +64,66 @@ local lspconfig = require("lspconfig");
 local null_ls = require("null-ls");
 
 null_ls.setup({
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.isort,
+	null_ls.builtins.formatting.stylua,
+	null_ls.builtins.formatting.prettier,
+	null_ls.builtins.formatting.black,
+	null_ls.builtins.formatting.isort,
 });
 
 local cmp = require("cmp");
 require("luasnip.loaders.from_vscode").lazy_load();
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<TAB>'] = cmp.mapping.select_next_item(),
-        ['<S-TAB>'] = cmp.mapping.select_prev_item(),
-        ['<Down>'] = function()
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<TAB>'] = cmp.mapping.select_next_item(),
+		['<S-TAB>'] = cmp.mapping.select_prev_item(),
+		['<Down>'] = function()
 
-        end,
-        ['<Up>'] = function()
+		end,
+		['<Up>'] = function()
 
-        end
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    }, {
-        { name = 'buffer' },
-    })
+		end
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+	}, {
+		{ name = 'buffer' },
+	})
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities();
 
 lspconfig.lua_ls.setup({ capabilities = capabilities });
 lspconfig.clangd.setup({
-    capabilities = capabilities,
+	capabilities = capabilities,
 });
 lspconfig.eslint.setup({ capabilities = capabilities });
 lspconfig.pyright.setup({ capabilities = capabilities });
 lspconfig.ts_ls.setup({ capabilities = capabilities });
+lspconfig.emmet_ls.setup({ capabilities = capabilities });
 
 -- godot shenanigans
 lspconfig.gdscript.setup({ capabilities = capabilities });
 vim.keymap.set("n", "<leader>sg", function()
-    vim.fn.serverstart("127.0.0.1:6004");
+	vim.fn.serverstart("127.0.0.1:6004");
 end, { noremap = true })
 
 local diagnostics = {
-    virtual_text = true, -- Disable builtin virtual text diagnostic
-    virtual_improved = {
-        current_line = 'only',
-    },
+	virtual_text = true, -- Disable builtin virtual text diagnostic
+	virtual_improved = {
+		current_line = 'only',
+	},
 }
 
 vim.diagnostic.config(diagnostics);
